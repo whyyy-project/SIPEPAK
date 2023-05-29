d299d020845c9b4ea30b93f3ad4757213d2f0d1a
Wahyu Nur Cahyo <wahyu241200@gmail.com>
2023-05-29 13:55:08 +0700

    edit cover

--- app/Views/layouts/public/cover.php
+++ app/Views/layouts/public/cover.php
@@ -21,16 +21,16 @@
         href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.1.3/assets/owl.carousel.min.css" />
 
     <!-- bootstrap core css -->
-    <link rel="stylesheet" type="text/css" href="css/bootstrap.css" />
+    <link rel="stylesheet" type="text/css" href="<?= base_URL(); ?>css/bootstrap.css" />
 
     <!-- fonts style -->
     <link href="https://fonts.googleapis.com/css?family=Open+Sans:400,700|Poppins:400,700&display=swap"
         rel="stylesheet">
     <!-- Custom styles for this template -->
-    <link href="css/style.css" rel="stylesheet" />
+    <link href="<?= base_URL(); ?>css/style.css" rel="stylesheet" />
     <!-- responsive style -->
-    <link href="css/responsive.css" rel="stylesheet" />
-    <link rel="shortcut icon" href="favicon.ico" />
+    <link href="<?= base_URL(); ?>css/responsive.css" rel="stylesheet" />
+    <link rel="shortcut icon" href="<?= base_URL(); ?>favicon.ico" />
 </head>
 
 <body>
@@ -580,49 +580,49 @@
     <!-- end  footer section -->
 
 
-    <script type="text/javascript" src="js/jquery-3.4.1.min.js"></script>
-    <script type="text/javascript" src="js/bootstrap.js"></script>
+    <script type="text/javascript" src="<?= base_URL(); ?>js/jquery-3.4.1.min.js"></script>
+    <script type="text/javascript" src="<?= base_URL(); ?>js/bootstrap.js"></script>
     <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.2.1/owl.carousel.min.js">
     </script>
     <script type="text/javascript">
-    $(".owl-carousel").owlCarousel({
-        loop: true,
-        margin: 10,
-        nav: true,
-        navText: [],
-        autoplay: true,
-        autoplayHoverPause: true,
-        responsive: {
-            0: {
-                items: 1
-            },
-            420: {
-                items: 2
-            },
-            1000: {
-                items: 5
+        $(".owl-carousel").owlCarousel({
+            loop: true,
+            margin: 10,
+            nav: true,
+            navText: [],
+            autoplay: true,
+            autoplayHoverPause: true,
+            responsive: {
+                0: {
+                    items: 1
+                },
+                420: {
+                    items: 2
+                },
+                1000: {
+                    items: 5
+                }
             }
-        }
 
-    });
+        });
     </script>
     <script>
-    var nav = $("#navbarSupportedContent");
-    var btn = $(".custom_menu-btn");
-    btn.click
-    btn.click(function(e) {
+        var nav = $("#navbarSupportedContent");
+        var btn = $(".custom_menu-btn");
+        btn.click
+        btn.click(function (e) {
 
-        e.preventDefault();
-        nav.toggleClass("lg_nav-toggle");
-        document.querySelector(".custom_menu-btn").classList.toggle("menu_btn-style")
-    });
+            e.preventDefault();
+            nav.toggleClass("lg_nav-toggle");
+            document.querySelector(".custom_menu-btn").classList.toggle("menu_btn-style")
+        });
     </script>
     <script>
-    $('.carousel').on('slid.bs.carousel', function() {
-        $(".indicator-2 li").removeClass("active");
-        indicators = $(".carousel-indicators li.active").data("slide-to");
-        a = $(".indicator-2").find("[data-slide-to='" + indicators + "']").addClass("active");
-    })
+        $('.carousel').on('slid.bs.carousel', function () {
+            $(".indicator-2 li").removeClass("active");
+            indicators = $(".carousel-indicators li.active").data("slide-to");
+            a = $(".indicator-2").find("[data-slide-to='" + indicators + "']").addClass("active");
+        })
     </script>
 
 </body>

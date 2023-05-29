e7f3af29900562b4d730ebdcda754585cb4ab5fe
Wahyu Nur Cahyo <wahyu241200@gmail.com>
2023-05-29 13:53:16 +0700

    edit cover

--- app/Views/layouts/public/cover.php
+++ app/Views/layouts/public/cover.php
@@ -133,7 +133,7 @@
                                                 User Guide
                                             </a>
                                             <a href="" class="btn-2">
-                                                Contact us
+                                                About
                                             </a>
                                         </div>
                                     </div>
@@ -585,44 +585,44 @@
     <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.2.1/owl.carousel.min.js">
     </script>
     <script type="text/javascript">
-        $(".owl-carousel").owlCarousel({
-            loop: true,
-            margin: 10,
-            nav: true,
-            navText: [],
-            autoplay: true,
-            autoplayHoverPause: true,
-            responsive: {
-                0: {
-                    items: 1
-                },
-                420: {
-                    items: 2
-                },
-                1000: {
-                    items: 5
-                }
+    $(".owl-carousel").owlCarousel({
+        loop: true,
+        margin: 10,
+        nav: true,
+        navText: [],
+        autoplay: true,
+        autoplayHoverPause: true,
+        responsive: {
+            0: {
+                items: 1
+            },
+            420: {
+                items: 2
+            },
+            1000: {
+                items: 5
             }
+        }
 
-        });
+    });
     </script>
     <script>
-        var nav = $("#navbarSupportedContent");
-        var btn = $(".custom_menu-btn");
-        btn.click
-        btn.click(function (e) {
+    var nav = $("#navbarSupportedContent");
+    var btn = $(".custom_menu-btn");
+    btn.click
+    btn.click(function(e) {
 
-            e.preventDefault();
-            nav.toggleClass("lg_nav-toggle");
-            document.querySelector(".custom_menu-btn").classList.toggle("menu_btn-style")
-        });
+        e.preventDefault();
+        nav.toggleClass("lg_nav-toggle");
+        document.querySelector(".custom_menu-btn").classList.toggle("menu_btn-style")
+    });
     </script>
     <script>
-        $('.carousel').on('slid.bs.carousel', function () {
-            $(".indicator-2 li").removeClass("active");
-            indicators = $(".carousel-indicators li.active").data("slide-to");
-            a = $(".indicator-2").find("[data-slide-to='" + indicators + "']").addClass("active");
-        })
+    $('.carousel').on('slid.bs.carousel', function() {
+        $(".indicator-2 li").removeClass("active");
+        indicators = $(".carousel-indicators li.active").data("slide-to");
+        a = $(".indicator-2").find("[data-slide-to='" + indicators + "']").addClass("active");
+    })
     </script>
 
 </body>

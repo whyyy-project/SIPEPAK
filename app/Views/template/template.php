494a26bb515fb00f359e0e0e3dbdf4381d8b1d87
Wahyu Nur Cahyo <116796627+whyyy24@users.noreply.github.com>
2023-06-03 17:31:32 +0700

    edit template

--- app/Views/template/template.php
+++ app/Views/template/template.php
@@ -209,14 +209,15 @@
     </div>
 
     <!-- Bootstrap core JavaScript-->
-    <script src="<?= base_URL(); ?>vendor/jquery/jquery.min.js"></script>
-    <script src="<?= base_URL(); ?>vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <script src="<?= base_URL(); ?>vendor_style/jquery/jquery.min.js"></script>
+    <script src="<?= base_URL(); ?>vendor_style/bootstrap/js/bootstrap.bundle.min.js"></script>
 
     <!-- Core plugin JavaScript-->
-    <script src="<?= base_URL(); ?>vendor/jquery-easing/jquery.easing.min.js"></script>
+    <script src="<?= base_URL(); ?>vendor_style/jquery-easing/jquery.easing.min.js"></script>
 
     <!-- Custom scripts for all pages-->
     <script src="<?= base_URL(); ?>js/sb-admin-2.min.js"></script>
+
 
 </body>
 

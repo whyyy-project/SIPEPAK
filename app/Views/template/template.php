3a78984449f69f23b71117a4a5cd9c50353b21fc
Wahyu Nur Cahyo <116796627+whyyy24@users.noreply.github.com>
2023-06-03 17:22:38 +0700

    add vendor

--- app/Views/template/template.php
+++ app/Views/template/template.php
@@ -15,7 +15,7 @@
 
     <!-- Custom fonts for this template-->
     <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
-    <link href="<?= base_URL(); ?>vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+    <link href="<?= base_URL(); ?>vendor_style/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
 
     <!-- Custom styles for this template-->
     <link href="<?= base_URL(); ?>css/sb-admin-2.min.css" rel="stylesheet">

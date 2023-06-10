2568abb08d2c63a344ab418be0348c2958ec7a2d
Wahyu Nur Cahyo <116796627+whyyy24@users.noreply.github.com>
2023-06-10 18:34:57 +0700

    custom dashboard

--- app/Views/layouts/user/dashboard.php
+++ app/Views/layouts/user/dashboard.php
@@ -5,22 +5,22 @@
 <div class="container-fluid">
     <!-- Page Heading -->
     <div class="d-sm-flex align-items-center justify-content-between mb-4">
-        <h1 class="h3 mb-0 text-gray-800">Dashboard <strong>User</strong></h1>
+        <h1 class="h3 mb-0 text-gray-800">Dashboard</h1>
     </div>
     <!-- end page heading -->
 
     <!-- Content Row -->
     <div class="row">
         <!-- Earnings (Monthly) Card Example -->
-        <div class="col-xl-3 col-md-6 mb-4">
+        <div class="col-xl-3 col-md-6 col-sm-6 mb-4">
             <div class="card border-left-primary shadow h-100 py-2">
                 <a href="<?= base_URL() ?>ajukan" style="text-decoration: none;">
                     <div class="card-body">
                         <div class="row no-gutters align-items-center">
                             <div class="col mr-2">
                                 <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">
-                                    Ajukan Proposal</div>
-                                <div class="h5 mb-0 font-weight-bold text-gray-800">Mulai Pengajuan</div>
+                                    Submit a Proposal</div>
+                                <div class="h5 mb-0 font-weight-bold text-gray-800">Start</div>
                             </div>
                             <div class="col-auto">
                                 <i class="fas fa-calendar fa-2x text-gray-300"></i>
@@ -31,7 +31,7 @@
             </div>
         </div>
         <!-- Earnings (Monthly) Card Example -->
-        <div class="col-xl-3 col-md-6 mb-4">
+        <div class="col-xl-3 col-md-6 col-sm-6 mb-4">
             <a href="<?= base_URL() ?>draft" style="text-decoration: none;">
 
                 <div class="card border-left-success shadow h-100 py-2">
@@ -39,8 +39,8 @@
                         <div class="row no-gutters align-items-center">
                             <div class="col mr-2">
                                 <div class="text-xs font-weight-bold text-success text-uppercase mb-1">
-                                    Draft Pengajuan</div>
-                                <div class="h5 mb-0 font-weight-bold text-gray-800">Lihat Draft</div>
+                                    Submission Draft</div>
+                                <div class="h5 mb-0 font-weight-bold text-gray-800">See Draft</div>
                             </div>
                             <div class="col-auto">
                                 <i class="fas fa-book fa-2x text-gray-300"></i>
@@ -52,13 +52,13 @@
         </div>
 
         <!-- Earnings (Monthly) Card Example -->
-        <div class="col-xl-3 col-md-6 mb-4">
+        <div class="col-xl-3 col-md-6 col-sm-6 mb-4">
             <a href="<?= base_URL() ?>daftar-pengajuan" style="text-decoration: none;">
                 <div class="card border-left-info shadow h-100 py-2">
                     <div class="card-body">
                         <div class="row no-gutters align-items-center">
                             <div class="col mr-2">
-                                <div class="text-xs font-weight-bold text-info text-uppercase mb-1">Daftar Pengajuan
+                                <div class="text-xs font-weight-bold text-info text-uppercase mb-1">Submission List
                                 </div>
                                 <div class="row no-gutters align-items-center">
                                     <div class="col-auto">
@@ -76,15 +76,15 @@
         </div>
 
         <!-- Pending Requests Card Example -->
-        <div class="col-xl-3 col-md-6 mb-4">
+        <div class="col-xl-3 col-md-6 col-sm-6 mb-4">
             <div class="card border-left-warning shadow h-100 py-2">
                 <a href="<?= base_URL() ?>riwayat" style="text-decoration: none;">
                     <div class="card-body">
                         <div class="row no-gutters align-items-center">
                             <div class="col mr-2">
                                 <div class="text-xs font-weight-bold text-warning text-uppercase mb-1">
-                                    Riwayat Pengajuan</div>
-                                <div class="h5 mb-0 font-weight-bold text-gray-800">Riwayat</div>
+                                    Submission History</div>
+                                <div class="h5 mb-0 font-weight-bold text-gray-800">History</div>
                             </div>
                             <div class="col-auto">
                                 <i class="fas fa-list fa-2x text-gray-300"></i>
@@ -97,6 +97,28 @@
 
     </div>
     <!-- end row -->
+
+    <div class="row">
+        <div class="col-md-7">
+            <div class="card shadow mb-4">
+                <div class="card-header py-3">
+                    <h6 class="m-0 font-weight-bold text-primary">Welcome to SIPEPAK</h6>
+                </div>
+                <div class="card-body">
+                    <h6>What is <strong>SIPEPAK</strong> ?</h6>
+                    <p>SIPEPAK (<strong>S</strong>istem <strong>I</strong>nformasi <strong>Pe</strong>ngajuan <strong>P</strong>roposal <strong>A</strong>nggaran <strong>K</strong>egiatan) is a revolutionary platform specifically designed to speed up and simplify the process of submitting proposals for Unugiri students.</p>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-5">
+            <div class="card shadow mb-4">
+                <div class="card-header py-3">
+                    <h6 class="m-0 font-weight-bold text-primary">User Guide</h6>
+                </div>
+                <iframe width="100%" height="250" src="https://www.youtube.com/embed/ur2hp_R-UhA" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+            </div>
+        </div>
+    </div>
 
 
 
